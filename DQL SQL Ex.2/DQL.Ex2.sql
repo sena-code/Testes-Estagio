061138b1ecfd96e7805c915d26eb457939716a1e
sena-code <sena.carbone@gmail.com>
2021-07-20 15:09:20 -0300

    SQL LEFT JOIN arrumado

--- DQL SQL Ex.2/DQL.Ex2.sql
+++ DQL SQL Ex.2/DQL.Ex2.sql
@@ -9,7 +9,7 @@ SELECT Nome,
  [Acressimo_10] = CASE WHEN idProd = idProduto THEN((Preco + Preco_Cor) * 0.10 + (Preco + Preco_Cor)) ELSE(Preco * 0.10 + Preco) END,
  [Desconto_10] = CASE WHEN idProd = idProduto THEN ((Preco + Preco_Cor) - 0.10 * (Preco + Preco_Cor)) ELSE(Preco - 0.10 * Preco) END,
  [Nome_Cores] = CASE WHEN idProd = idProduto THEN(Nomes_Cores) ELSE('Cor Indisponível') END
-FROM Produtos, Cores WHERE (Nome) IS NOT NULL ORDER BY Acressimo_10;
+FROM Produtos LEFT JOIN Cores ON Produtos.idProduto = Cores.idProd WHERE  (Nome) IS NOT NULL ORDER BY Acressimo_10;
 
 /*Da forma do exercício */
 
@@ -18,7 +18,7 @@ SELECT NOME_PROD,
  [Acressimo_10] = CASE WHEN IDPRODUTO = IDPRODUTO THEN((PRC_PROD + PRC_COR) * 0.10 + (PRC_PROD + PRC_COR)) ELSE(PRC_PROD * 0.10 + PRC_PROD) END,
  [Desconto_10] = CASE WHEN IDPRODUTO = IDPRODUTO THEN (PRC_PROD + PRC_COR) - 0.10 * (PRC_PROD + PRC_COR)) ELSE(PRC_PROD - 0.10 * PRC_PROD) END,
  [Nome_Cores] = CASE WHEN IDPRODUTO = IDPRODUTO THEN(NOME_COR) ELSE('Cor Indisponível') END
-FROM Produtos, Cores WHERE (NOME_PROD) IS NOT NULL ORDER BY Preco;
+FROM Produtos LEFT JOIN Cores ON Produtos.IDPRODUTO = Cores.IDPRODUTO WHERE (NOME_PROD) IS NOT NULL ORDER BY Preco;
 
 
 
